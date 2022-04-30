af43cce068bc250440d1fd7166ec1f2519503b30
LieutenantDan1 <48690122+LieutenantDan1@users.noreply.github.com>
2022-04-30 03:17:20 -0400

    Add files via upload
    made error less annoying to construct
    updates oopsies in operator descriptions

--- expression.py
+++ expression.py
@@ -3,21 +3,57 @@ from typing import Any, Optional, Union
 
 class Error:
     """
+    A traceback of errors.
 
+    traceback: A list of errors, where the first item represents the origin of
+        the error, and each subsequent error is one level higher.
     """
     traceback: list[str]
 
-    def __init__(self, traceback: list[str]):
-        self.traceback = traceback
+    def __init__(self, message: str = "", origin: int = -1):
+        self.traceback = []
+        if len(message) != 0:
+            self.append(message, origin)
     
     def __repr__(self):
-        return '\n'.join(self.traceback)
+        """
+        Return all traceback messages on separate lines, where each line starts
+        with the same number of spaces as the level of the error in the
+        expression tree.
 
-    def append(self, message: str, origin: int):
+        >>> e = Error("ADD operator requires exactly 2 arguments.")
+        >>> e.append("", 0)
+        >>> e.append("", 1)
+        >>> e.append("")
+        >>> e.append("", 3)
+        >>> e
+        Line 3:
+         ---
+          Line 1:
+           Line 0:
+            ADD operator requires exactly 2 arguments.
+        """
+        out = []
+        for i in range(1, len(self.traceback) + 1):
+            msg = self.traceback[-i]
+            if len(msg) == 0:
+                out.append(f"{' ' * (i - 1)}---")
+            else:
+                out.append(f"{' ' * (i - 1)}{msg}")
+        return '\n'.join(out)
+
+    def append(self, message: str, origin: int = -1):
+        """
+        Append an error to the traceback. If origin >= 0, include a line
+        indicator at the start of the message.
+        """
         if origin < 0:
             self.traceback.append(message)
         else:
-            self.traceback.append(f"Line {origin}: {message}")
+            if len(message) != 0:
+                self.traceback.append(f"Line {origin}: {message}")
+            else:
+                self.traceback.append(f"Line {origin}:")
 
 
 def add(args: list[Union[int, float]],
@@ -29,61 +65,62 @@ def add(args: list[Union[int, float]],
     <env> is ignored.
     """
     if len(args) != 2:
-        return Error(["ADD operator requires exactly 2 arguments."])
+        return Error("ADD operator requires exactly 2 arguments.")
     try:
         return args[0] + args[1]
     except TypeError as te:
-        return Error([str(te)])
+        return Error(str(te))
 
 
 def sub(args: list[Union[int, float]],
         env: dict[str, Any]) -> Union[int, float, Error]:
     """
     Throws unless <args> is a list of 2 objects.
-    Throws unless both objects in <args> may be added.
+    Throws unless both objects in <args> may be subtracted.
     Subtracts two arguments in <args> and return their difference.
     <env> is ignored.
     """
     if len(args) != 2:
-        return Error(["SUB operator requires exactly 2 arguments."])
+        return Error("SUB operator requires exactly 2 arguments.")
     try:
         return args[0] - args[1]
     except TypeError as te:
-        return Error([str(te)])
+        return Error(str(te))
 
 
 def mul(args: list[Union[int, float]],
         env: dict[str, Any]) -> Union[int, float, Error]:
     """
     Throws unless <args> is a list of 2 objects.
-    Throws unless both objects in <args> may be added.
+    Throws unless both objects in <args> may be multiplied.
     Multiplies two arguments in <args> and return their product.
     <env> is ignored.
     """
     if len(args) != 2:
-        return Error(["MUL operator requires exactly 2 arguments."])
+        return Error("MUL operator requires exactly 2 arguments.")
     try:
         return args[0] * args[1]
     except TypeError as te:
-        return Error([str(te)])
+        return Error(str(te))
 
 
 def div(args: list[Union[int, float]],
         env: dict[str, Any]) -> Union[int, float, Error]:
     """
     Throws unless <args> is a list of 2 objects.
-    Throws unless both objects in <args> may be added.
+    Throws unless both objects in <args> may be divided.
+    Throws if args[1] is 0.
     Divides two arguments in <args> and return their quotient.
     <env> is ignored.
     """
     if len(args) != 2:
-        return Error(["DIV operator requires exactly 2 arguments."])
+        return Error("DIV operator requires exactly 2 arguments.")
     try:
         return args[0] / args[1]
     except TypeError as te:
         return Error([str(te)])
     except ZeroDivisionError as zde:
-        return Error([str(zde)])
+        return Error(str(zde))
 
 
 def assign(args: list[Any], env: dict[str, Any]) -> None:
@@ -93,9 +130,9 @@ def assign(args: list[Any], env: dict[str, Any]) -> None:
     Assign a given value (args[1]) to a given reference (args[0]) in <env>.
     """
     if len(args) != 2:
-        return Error(["ASSIGN operator requires exactly 2 arguments."])
+        return Error("ASSIGN operator requires exactly 2 arguments.")
     if not hasattr(args[0], 'assign'):
-        return Error(["ASSIGN operator requires name as left operand."])
+        return Error("ASSIGN operator requires name as left operand.")
     args[0].assign(args[1], env)
 
 
@@ -187,9 +224,7 @@ class Name(Expression):
         5
         """
         if self.name not in env:
-            error = Error([])
-            error.append(f"Name \'{self.name}\' not defined.", self.origin)
-            return error
+            return Error(f"Name \'{self.name}\' not defined.", self.origin)
         else:
             return env[self.name]
 
@@ -227,18 +262,14 @@ class Operation(Expression):
         Missing left operand.
         """
         if self.operator not in OPERATORS:
-            error = Error([])
-            error.append(f"Operator \'{self.operator}\' is invalid.",
+            return Error(f"Operator \'{self.operator}\' is invalid.",
                          self.origin)
-            return error
         operator = OPERATORS[self.operator]
         args = []
 
         if operator[0]: # LEFT OPERAND
             if self.l_operand is None:
-                error = Error([])
-                error.append(f"Missing left operand.", self.origin)
-                return error
+                return Error(f"Missing left operand.", self.origin)
             value = self.l_operand.evaluate(env)
             if isinstance(value, Error):
                 value.append("", self.origin)
@@ -247,9 +278,7 @@ class Operation(Expression):
         
         if operator[1]: # RIGHT OPERAND
             if self.r_operand is None:
-                error = Error([])
-                error.append(f"Missing right operand.", self.origin)
-                return error
+                return Error(f"Missing right operand.", self.origin)
             value = self.r_operand.evaluate(env)
             if isinstance(value, Error):
                 value.append("", self.origin)
