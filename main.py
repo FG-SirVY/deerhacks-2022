2a9be4553efbe47172d2675a854e9f2b833825eb
MMqd <85073526+MMqd@users.noreply.github.com>
2022-04-30 23:18:01 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -24,17 +24,24 @@ def run_file(fname: str, env: Environment) -> None:
     432
     >>> run_file("test-scripts/print.pain", prepare_environment())
     5
-    5.0
-    5.0
-    Test
+    5
     >>> run_file("test-scripts/min_max_print.pain", prepare_environment())
     6
     >>> run_file("test-scripts/complex_arithmetic_and_builtins.pain", prepare_environment())
     432
     >>> run_file("test-scripts/if_stmt.pain", prepare_environment())
     10
-    >>> run_file("test-scripts/assign_print_test.pain", prepare_environment())
+    >>> run_file("test-scripts/more_tests.pain", prepare_environment())
+    print
+    a is less than b
+    >>> run_file("test-scripts/reuse_test.pain", prepare_environment())
     3
+    >>> run_file("test-scripts/and.pain", prepare_environment())
+    1
+    0
+    >>> run_file("test-scripts/or.pain", prepare_environment())
+    1
+    1
     """
     expressions: list[Expression] = []
 
